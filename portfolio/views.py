b95bc208614ca7fec6b866700961a8c8496aa10e
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-11-28 17:49:09 -0500

    fixed

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-projects = [
-	{
-		'name': 'Shinobi Network',
-		'link': 'github.pinosaur/shinobi-network',
-		'description': 'multiplayer shooter created with libgdx'
-	},
-	{
-		'name': 'Soulless',
-		'link': 'github.pinosaur/soulless',
-		'description': 'platformer created for ludum dare 45'
-	}
-]
+
 
 def home(request):
 
 	return render(request, 'portfolio/home.html')
 
 def projects(request):
+
+	projects = [
+		{
+			'name': 'Shinobi Network',
+			'link': 'github.pinosaur/shinobi-network',
+			'description': 'multiplayer shooter created with libgdx'
+		},
+		{
+			'name': 'Soulless',
+			'link': 'github.pinosaur/soulless',
+			'description': 'platformer created for ludum dare 45'
+		}
+	]
+
 	context = {
 		'projects': projects
 	}
-
-	print(context)
 
 	return render(request, 'portfolio/projects.html', context)
