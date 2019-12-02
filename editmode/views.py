9fa86ee00a85c186dccb4b16bc22d92d231000b1
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-12-01 21:49:43 -0500

    create proejct form;

--- editmode/views.py
+++ editmode/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 @login_required
 def createproject(request):
-	#REDIRECT TO PROPER LOGIN PAGE!!!!!!
-	return render(request, 'editmode/createproject.html')
+	form = forms.NewProjectForm()
+	return render(request, 'editmode/createproject.html',{'form':form})
