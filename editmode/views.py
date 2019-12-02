65fd509d73382a787979e3d23afa22453199b3b7
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-12-01 22:03:46 -0500

    form submission and flash messages

--- editmode/views.py
+++ editmode/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from . import forms
+from django.contrib import messages
 
 # Create your views here.
 @login_required
 def createproject(request):
-	form = forms.NewProjectForm()
-	return render(request, 'editmode/createproject.html',{'form':form})
+	#TODO: MOVE THE LOGIN REDIRECT FROM THE ADMIN TO A DEDICATED FORM
+	if request.method =='POST': # if a post request was recieved
+		form = forms.NewProjectForm(request.POST)
+		if form.is_valid(): #validate that the data we got was actually from the form
+			name = form.cleaned_data.get('name')
+			messages.success(request, f'{name} successfully created!') #display a msg
+			return redirect('portfolio-projects')
+	else:
+		form = forms.NewProjectForm()
+
+
+	return render(request, 'editmode/createproject.html', {'form':form})
