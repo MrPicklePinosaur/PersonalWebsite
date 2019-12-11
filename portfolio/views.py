ed11239a5141be5377589b49a3f40a5727398c9c
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-12-11 13:14:22 -0500

    url dispatcher

--- portfolio/views.py
+++ portfolio/views.py
@@ -19,3 +19,12 @@ def projects(request):
 
 	return render(request, 'portfolio/projects.html', context)
 
+def project_info(request,slug):
+
+	#determine if slug was valid
+
+	context = {
+		'project_info': slug
+	}
+	
+	return render(request, 'portfolio/projectinfo.html', context)
